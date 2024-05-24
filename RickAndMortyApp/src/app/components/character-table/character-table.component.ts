b7d8c235fdcc53c3e63e25f042b5cd3795d57012
tanqueta88 <oliverprim783@gmail.com>
2024-05-23 23:28:29 -0300

    character-table.component.ts file was modified

--- RickAndMortyApp/src/app/components/character-table/character-table.component.ts
+++ RickAndMortyApp/src/app/components/character-table/character-table.component.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './character-table.component.html',
   styleUrl: './character-table.component.scss'
 })
-export class CharacterTableComponent {
+export class CharacterTableComponent implements OnInit {
+  characters: any[] = [];
 
-}
+  constructor(private characterService: CharacterService) { }
+
+  ngOnInit(): void {
+    this.loadCharacters();
+  }
+
+  loadCharacters() {
+    this.characterService.getCharacters().subscribe(
+      (response: ApiResponse<Character>) => {
+        this.characters = this.transformCharacterData(response.results);
+      },
+      error => {
+        console.log('Error fetching characters:', error);
+      }
+    );
+  }
