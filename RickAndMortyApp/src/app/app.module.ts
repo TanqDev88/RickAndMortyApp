37603adfcc147595dfb4702c903c63e6869f2346
tanqueta88 <oliverprim783@gmail.com>
2024-07-10 16:48:13 -0300

    Home component is added.

--- RickAndMortyApp/src/app/app.module.ts
+++ RickAndMortyApp/src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistrationComponent } from './components/view/registration/registration.component';
+import { HttpClientModule } from '@angular/common/http';
 import { CharacterTableComponent } from './components/character-table/character-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
+    AdminComponent,
+    HomeComponent,
+    RegistrationComponent,
+    CharacterTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
