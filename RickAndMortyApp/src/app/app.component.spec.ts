d27bcadfff012431ba09bd1b3088eb0930ed1d1e
tanqueta88 <oliverprim783@gmail.com>
2024-07-12 20:25:32 -0300

    Unit test are added to te app component

--- RickAndMortyApp/src/app/app.component.spec.ts
+++ RickAndMortyApp/src/app/app.component.spec.ts
@@ -23,6 +23,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('RickAndMortyApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('RickAndMortyApp');
   });
 });
